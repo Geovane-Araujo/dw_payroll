ab40ceebec3264b103fd764ba7e6866ea07c7b10
Geovane Araújo <geovane-araujo@outlook.com>
2022-03-15 21:07:19 -0300

    concluido criação de nomes de pessoas randomicamente

--- app util/PopulateNames/src/main/java/br/com/populateNames/Main.java
+++ app util/PopulateNames/src/main/java/br/com/populateNames/Main.java
@@ -1,5 +1,6 @@
 package br.com.populateNames;
 
+import br.com.populateNames.model.Nomes;
 import br.com.populateNames.service.NomesService;
 import br.com.populateNames.util.Util;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,33 +11,51 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Random;
 
 
 public class Main {
 
     public static final String DIR_JSON = "/home/geovane/Documentos/Workspace/Back-end/JAva/dw_payroll/dados_brutos/candidatos";
+    static NomesService nomesService = new NomesService();
+
+
 
     public static void main(String[] args){
         try {
             readJson();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-    static void readJson() throws IOException {
+    static void readJson() throws Exception {
         List<String> arquivos = new ArrayList<>();
         ObjectMapper objectMapper = new ObjectMapper();
         arquivos = listaArquivos();
 
-        for(String fileName: arquivos){
-            Map<String,Object> obj = objectMapper.readValue(new File(DIR_JSON + "/"+fileName),Map.class);
-            System.out.printf(obj.toString());
+        List<Map<String, Object>> jsonCarregados = new ArrayList<>();
 
+        for (String fileName: arquivos){
+            Map<String,Object> obj = objectMapper.readValue(new File(DIR_JSON + "/"+fileName),Map.class);
+            jsonCarregados.add(obj);
         }
 
+
+        for(Map obj: jsonCarregados){
+            String[] nomeSplit = obj.get("nomeCompleto").toString().split(" ");
+            String primeiroNome = nomeSplit[0];
+
+            for(Map objSob: jsonCarregados){
+                Nomes nomes = new Nomes();
+                String[] sobrenomeSplit = objSob.get("nomeCompleto").toString().split(" ");
+                primeiroNome = nomeSplit[0] + formaSobrenome(sobrenomeSplit);
+
+                nomes.setNome(primeiroNome);
+                nomesService.save(nomes);
+            }
+        }
 
     }
 
@@ -50,5 +69,17 @@ public class Main {
         }
 
         return listaArquivos;
+    }
+
+    static String formaSobrenome(String[] sobrenomes){
+
+        String sobrenome = "";
+        Random random = new Random();
+
+        for (int i = 1;i < sobrenomes.length;i++){
+            int rn = random.nextInt((sobrenomes.length - 1)) + 1;
+            sobrenome += " " + sobrenomes[rn];
+        }
+        return sobrenome;
     }
 }
--- app util/PopulateNames/src/main/java/br/com/populateNames/model/Nomes.java
+++ app util/PopulateNames/src/main/java/br/com/populateNames/model/Nomes.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@TableName("nomes_variados")
+@TableName("person")
 public class Nomes {
 
     @Id
